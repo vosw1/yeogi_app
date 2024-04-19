171a03c9ad59537043f71992bc58bbbdf67d5e7c
vosw1 <vosw1@naver.com>
2024-04-19 17:54:00 +0900

    각 페이지 클래스 생성

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:yogi_project/size.dart';
 
 import '../home_page/_header/home_header.dart';
 import '../home_page/body/home_body.dart';
+import 'main_holder.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -29,7 +30,6 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: HomeBottomNavigationBar(),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
@@ -69,48 +69,4 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class HomeBottomNavigationBar extends StatefulWidget {
-  @override
-  State<HomeBottomNavigationBar> createState() =>
-      _HomeBottomNavigationBarState();
-}
 
-class _HomeBottomNavigationBarState extends State<HomeBottomNavigationBar> {
-  int _selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      backgroundColor: Colors.white,
-      type: BottomNavigationBarType.fixed,
-      currentIndex: _selectedIndex,
-      onTap: (index) {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      items: [
-        const BottomNavigationBarItem(
-          label: '홈',
-          icon: Icon(CupertinoIcons.house_fill),
-        ),
-        const BottomNavigationBarItem(
-          label: '검색',
-          icon: Icon(CupertinoIcons.search),
-        ),
-        const BottomNavigationBarItem(
-          label: '주변',
-          icon: Icon(CupertinoIcons.placemark_fill),
-        ),
-        const BottomNavigationBarItem(
-          label: '찜',
-          icon: Icon(CupertinoIcons.heart_fill),
-        ),
-        const BottomNavigationBarItem(
-          label: '내 정보',
-          icon: Icon(CupertinoIcons.smiley_fill),
-        ),
-      ],
-    );
-  }
-}
