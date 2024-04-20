ed40d902245f51c37a3cf4f141fe005a2b8655ff
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-04-21 00:52:22 +0900

    마이페이지 헤더 배너 완성

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:yogi_project/my_page/my_page_header_banner.dart';
 
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Scaffold(
+      body: ListView(
+        children: [
+          MyPageHeaderBanner(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/pages/scarp_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScrapPage extends StatelessWidget {
+  const ScrapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
