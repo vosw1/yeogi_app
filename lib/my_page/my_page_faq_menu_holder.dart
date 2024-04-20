06f4eae0a88bc92f6729d4de5077f0cec86c5f8e
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-04-21 04:18:18 +0900

    [fix] 마이페이지 최종본

--- lib/my_page/my_page_faq_menu_holder.dart
+++ lib/my_page/my_page_faq_menu_holder.dart
@@ -8,32 +8,44 @@ class MyPageFaqMenuHolder extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
-      itemCount: myPageMenu.length,
+      itemCount: myPageMenu.length + 1,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Icon(myPageMenu[index].iconData),
-          title: Text(myPageMenu[index].title),
-          trailing: (index == 1 || index == 2) ? Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                child: Text(
-                  '오전 9시 ~ 새벽 3시',
-                  style: TextStyle(fontSize: 12),
-                ),
-              ),
-              Icon(Icons.arrow_forward_ios),
-            ],
-          ) : Icon(Icons.arrow_forward_ios),
-          onTap: () {
+        if (index == 0) {
 
-          },
-        );
+          return ListTile(
+            title: Text('고객센터', style: TextStyle(fontWeight: FontWeight.bold)),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+
+            },
+          );
+        } else {
+
+          return ListTile(
+            leading: Icon(myPageMenu[index - 1].iconData), // 인덱스 조정
+            title: Text(myPageMenu[index - 1].title),
+            trailing: (index == 2 || index == 3) ? Row( // 인덱스 조정으로 1:1 카카오 문의와 고객 행복센터 연결에만 특별한 트레일링 추가
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Text(
+                    '오전 9시 ~ 새벽 3시',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios),
+              ],
+            ) : Icon(Icons.arrow_forward_ios),
+            onTap: () {
+
+            },
+          );
+        }
       },
     );
   }
