c460719b8f10437850a6403a5590c94ae3210098
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-04-21 01:36:55 +0900

    [fix] 로그인 버튼

--- lib/my_page/my_page_login_button.dart
+++ lib/my_page/my_page_login_button.dart
@@ -10,21 +10,21 @@ class MyPageLoginButton extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Expanded( // Expanded 위젯 사용
-            child: Padding( // Padding 위젯 추가
-              padding: const EdgeInsets.only(top: gap_s, left: gap_s, right: gap_s), // 양옆 패딩 설정
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: gap_s, left: gap_s, right: gap_s),
               child: InkWell(
                 onTap: () {
-                  // 로그인/회원가입 로직 추가
+                  print("버튼 클릭됨");
                 },
                 child: Container(
                   height: MediaQuery.of(context).size.height * 0.07,
                   width: 15,
-                  alignment: Alignment.center, // 컨테이너 내부의 텍스트를 중앙에 정렬
+                  alignment: Alignment.center,
                   child: Text(
                     "회원가입 / 로그인",
                     style: TextStyle(fontSize: 17, color: Colors.white),
-                    textAlign: TextAlign.center, // 텍스트 중앙 정렬
+                    textAlign: TextAlign.center,
                   ),
                   decoration: BoxDecoration(
                     color: Colors.blue,
