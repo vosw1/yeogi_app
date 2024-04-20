164fcd7b0cc45fe33e5e6038af62be130fd2f4ad
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-04-21 04:05:04 +0900

    [fix]마이페이지 수정

--- lib/my_page/my_page_faq_menu_holder.dart
+++ lib/my_page/my_page_faq_menu_holder.dart
@@ -8,24 +8,30 @@ class MyPageFaqMenuHolder extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
-      itemCount: myPageMenu.length + 1, // 고객센터 항목을 포함하기 위해 +1
+      itemCount: myPageMenu.length,
       itemBuilder: (BuildContext context, int index) {
-        // 첫 번째 인덱스일 때 고객센터 ListTile 반환
-        if (index == 0) {
-          return ListTile(
-            title: Text('고객센터', style: TextStyle(fontWeight: FontWeight.bold)),
-            onTap: () {
-              // 고객센터 탭 시 수행할 동작
-            },
-          );
-        }
-        // 그 외의 경우, myPageMenu 항목 반환 (인덱스 조정 필요)
         return ListTile(
-          leading: Icon(myPageMenu[index - 1].iconData), // 인덱스를 1 감소시켜 조정
-          title: Text(myPageMenu[index - 1].title), // 인덱스를 1 감소시켜 조정
-          trailing: Icon(Icons.arrow_forward_ios),
+          leading: Icon(myPageMenu[index].iconData),
+          title: Text(myPageMenu[index].title),
+          trailing: (index == 1 || index == 2) ? Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Text(
+                  '오전 9시 ~ 새벽 3시',
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+              Icon(Icons.arrow_forward_ios),
+            ],
+          ) : Icon(Icons.arrow_forward_ios),
           onTap: () {
-            // 각 메뉴 탭 시 수행할 동작
+
           },
         );
       },
