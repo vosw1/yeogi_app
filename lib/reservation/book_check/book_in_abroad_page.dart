d4d3a9569f16915aa9ece0b1527c8ac3699892ed
vosw1 <vosw1@naver.com>
2024-04-23 17:52:57 +0900

    국내 해외 예약내역 리스트로 구현함

--- lib/reservation/book_check/book_in_abroad_page.dart
+++ lib/reservation/book_check/book_in_abroad_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../../components/common/book_item_data.dart';
 import '../../size.dart';
 import '../../style.dart';
 
 class BookInAbroadPage extends StatelessWidget {
-  final List<BookItemData> bookInAbroardList; // 전체 목록을 받도록 수정
+  final List<BookItemData> bookInAbroardList;
 
-  BookInAbroadPage({required this.bookInAbroardList}); // 생성자 수정
+  BookInAbroadPage({required this.bookInAbroardList});
 
   @override
   Widget build(BuildContext context) {
@@ -18,49 +17,60 @@ class BookInAbroadPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(gap_m),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView( // Wrap the Column with a ListView
           children: [
-            Text("이용 ${bookInAbroardList.length}건", style: h6()), // 예약 데이터는 하나씩만 전달되므로 일단 "이용 1건"으로 고정
-            SizedBox(height: gap_s),
-            Divider(),
-            // 각 예약 데이터 항목에 대한 UI를 반복하여 빌드합니다.
-            for (var bookItemData in bookInAbroardList)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12.0),
-                        child: Image.asset(
-                          bookItemData.imagePath,
-                          width: 150,
-                          height: 150,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(width: gap_s), // Divider between image and text
-                      Expanded(
-                        flex: 2,
-                        child: Column(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("이용 ${bookInAbroardList.length}건", style: h6()),
+                SizedBox(height: gap_s),
+                Divider(),
+                // Display reservation items using ListView.builder
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: bookInAbroardList.length,
+                  itemBuilder: (context, index) {
+                    final bookItemData = bookInAbroardList[index];
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('${bookItemData.stayName}', style: h4()),
-                            Text('${bookItemData.roomName}  |  ${bookItemData.location}', style: subtitle2(mColor: Colors.grey)),
-                            SizedBox(height: gap_m),
-                            Text('체크인 ${bookItemData.checkInDate}  |  체크아웃 ${bookItemData.checkOutDate}', style: overLine()),
-                            SizedBox(height: gap_xx),
-                            Text('인원 ${bookItemData.personCount}  |  가격 ${bookItemData.price}', style: overLine()),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12.0),
+                              child: Image.asset(
+                                bookItemData.imagePath,
+                                width: 150,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(width: gap_s),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('${bookItemData.stayName}', style: h4()),
+                                  Text('${bookItemData.roomName}  |  ${bookItemData.location}', style: subtitle2(mColor: Colors.grey)),
+                                  SizedBox(height: gap_m),
+                                  Text('체크인 ${bookItemData.checkInDate}  |  체크아웃 ${bookItemData.checkOutDate}', style: overLine()),
+                                  SizedBox(height: gap_xx),
+                                  Text('인원 ${bookItemData.personCount}  |  가격 ${bookItemData.price}', style: overLine()),
+                                ],
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Divider(), // Divider between items
-                ],
-              ),
+                        Divider(),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
--- lib/reservation/book_check/book_in_korea_page.dart
+++ lib/reservation/book_check/book_in_korea_page.dart
@@ -4,9 +4,9 @@ import 'package:yogi_project/style.dart';
 import '../../components/common/book_item_data.dart';
 
 class BookInKoreaPage extends StatelessWidget {
-  final List<BookItemData> bookInKoreaList; // 전체 목록을 받도록 수정
+  final List<BookItemData> bookInKoreaList;
 
-  BookInKoreaPage({required this.bookInKoreaList}); // 생성자 수정
+  BookInKoreaPage({required this.bookInKoreaList});
 
   @override
   Widget build(BuildContext context) {
@@ -16,50 +16,61 @@ class BookInKoreaPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(gap_m),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView( // Wrap the Column with a ListView
           children: [
-            Text("이용 ${bookInKoreaList.length}건", style: h6()), // 예약 데이터는 하나씩만 전달되므로 일단 "이용 1건"으로 고정
-            SizedBox(height: gap_s),
-            Divider(),
-            // 각 예약 데이터 항목에 대한 UI를 반복하여 빌드합니다.
-            for (var bookItemData in bookInKoreaList)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12.0),
-                        child: Image.asset(
-                          bookItemData.imagePath,
-                          width: 150,
-                          height: 150,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(width: gap_s), // Divider between image and text
-                      Expanded(
-                        flex: 2,
-                        child: Column(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("이용 ${bookInKoreaList.length}건", style: h6()),
+                SizedBox(height: gap_s),
+                Divider(),
+                // Display reservation items using ListView.builder
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: bookInKoreaList.length,
+                  itemBuilder: (context, index) {
+                    final bookItemData = bookInKoreaList[index];
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            SizedBox(height: gap_m),
-                            Text('${bookItemData.stayName}', style: h4()),
-                            Text('${bookItemData.roomName}  |  ${bookItemData.location}', style: subtitle2(mColor: Colors.grey)),
-                            SizedBox(height: gap_m),
-                            Text('체크인 ${bookItemData.checkInDate}  |  체크아웃 ${bookItemData.checkOutDate}', style: overLine()),
-                            SizedBox(height: gap_xx),
-                            Text('인원 ${bookItemData.personCount}  |  가격 ${bookItemData.price}', style: overLine()),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12.0),
+                              child: Image.asset(
+                                bookItemData.imagePath,
+                                width: 150,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(width: gap_s),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: gap_m),
+                                  Text('${bookItemData.stayName}', style: h4()),
+                                  Text('${bookItemData.roomName}  |  ${bookItemData.location}', style: subtitle2(mColor: Colors.grey)),
+                                  SizedBox(height: gap_m),
+                                  Text('체크인 ${bookItemData.checkInDate}  |  체크아웃 ${bookItemData.checkOutDate}', style: overLine()),
+                                  SizedBox(height: gap_xx),
+                                  Text('인원 ${bookItemData.personCount}  |  가격 ${bookItemData.price}', style: overLine()),
+                                ],
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Divider(), // Divider between items
-                ],
-              ),
+                        Divider(),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
