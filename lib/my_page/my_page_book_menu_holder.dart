435bec0134b5b48f2e7d4f6de77113ef384c9e86
vosw1 <vosw1@naver.com>
2024-04-23 12:50:20 +0900

    예약 페이지 만들기 위해 마이페이지에서 버튼 클릭시 페이지로 이동

--- lib/my_page/my_page_book_menu_holder.dart
+++ lib/my_page/my_page_book_menu_holder.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yogi_project/reservation/reservation_check/reservation_in_abroad_page.dart';
+import 'package:yogi_project/reservation/reservation_check/reservation_in_korea_page.dart';
+import 'package:yogi_project/size.dart';
 
 class MyPageBookMenuHolder extends StatelessWidget {
   const MyPageBookMenuHolder({super.key});
@@ -9,22 +12,27 @@ class MyPageBookMenuHolder extends StatelessWidget {
       children: <Widget>[
         ListTile(
           title: Text('예약내역', style: TextStyle(fontWeight: FontWeight.bold)),
-          onTap: () {
-
-          },
         ),
         ListTile(
           title: Text('국내숙소'),
           trailing: Icon(Icons.arrow_forward_ios),
           onTap: () {
-
+            // 국내숙소 페이지로 이동
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ReservationInKoreaPage(reservationItemsData: [],)),
+            );
           },
         ),
         ListTile(
           title: Text('해외숙소'),
           trailing: Icon(Icons.arrow_forward_ios),
           onTap: () {
-
+            //해외숙소 페이지로 이동
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ReservationInAbroadPage()),
+            );
           },
         ),
       ],
