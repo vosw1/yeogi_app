20c668e2413829152c5f8ae1248cacfaa11da83b
vosw1 <vosw1@naver.com>
2024-05-08 17:35:05 +0900

    수동으로 gps 설정

--- lib/ui/pages/surroundings/near_from_me_page.dart
+++ lib/ui/pages/surroundings/near_from_me_page.dart
@@ -4,7 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:yogi_project/_core/constants/size.dart'; // 위치 서비스를 사용하기 위한 패키지
+import 'package:yogi_project/_core/constants/size.dart';
 
 class NearFromMePage extends StatefulWidget {
   @override
@@ -15,39 +15,52 @@ class _NearFromMePageState extends State<NearFromMePage> {
   String _searchText = '';
   List<String> searchResults = [];
   late Completer<GoogleMapController> _controllerCompleter;
-  late LatLng _currentPosition; // 현재 위치를 저장할 변수
-
-  final CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(35.1796, 129.0756), // 부산을 기본 위치로 설정
-    zoom: 12,
-  );
-
+  LatLng? _currentPosition;
   final Set<Marker> markers = {};
 
   @override
   void initState() {
     super.initState();
     _controllerCompleter = Completer<GoogleMapController>();
-    _getCurrentLocation(); // initState에서 현재 위치 가져오기
+    _getCurrentLocation(); // 앱이 시작할 때 현재 위치 가져오기
   }
 
   // 위치 가져오기
   Future<void> _getCurrentLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    setState(() {
-      _currentPosition = LatLng(position.latitude, position.longitude);
-    });
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.denied) {
+        Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.medium,
+        );
+        setState(() {
+          _currentPosition = LatLng(position.latitude, position.longitude);
+          if (_currentPosition != null) {
+            addMarker(_currentPosition!); // 현재 위치에 마커 추가
+            _goToCurrentPosition(); // 현재 위치로 지도 이동
+          }
+        });
+      } else {
+        // 위치 권한이 거부된 경우
+        // 여기에 사용자에게 권한을 다시 요청하는 로직을 추가할 수 있습니다.
+      }
+    } catch (e) {
+      print('위치 가져오기 오류: $e');
+      // 위치 가져오기에 실패한 경우에 대한 처리 추가
+    }
   }
 
   void addMarker(LatLng coordinate) {
-    int id = Random().nextInt(100);
+    final int id = Random().nextInt(100); // 랜덤한 ID 생성
+    final MarkerId markerId = MarkerId(id.toString()); // 마커 ID 생성
+
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: coordinate,
+    );
 
     setState(() {
-      markers.add(Marker(
-        position: coordinate,
-        markerId: MarkerId(id.toString()),
-      ));
+      markers.add(marker); // 마커 추가
     });
   }
 
@@ -98,21 +111,20 @@ class _NearFromMePageState extends State<NearFromMePage> {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(gap_s),
                 child: GoogleMap(
-                  initialCameraPosition: _initialPosition,
+                  initialCameraPosition: _currentPosition != null
+                      ? CameraPosition(target: _currentPosition!, zoom: 12)
+                      : CameraPosition(target: LatLng(35.1796, 129.0756), zoom: 12), // 부산을 기본 위치로 설정
                   onMapCreated: (controller) async {
                     _controllerCompleter.complete(controller);
                   },
-                  onTap: addMarker,
                   markers: markers,
-                  myLocationEnabled: true, // 사용자의 현재 위치를 표시
-                  myLocationButtonEnabled: true, // 사용자의 현재 위치로 이동하는 버튼 표시
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
                 ),
               ),
             ),
             SizedBox(height: gap_m),
-            Text(
-              '결과 ${searchResults.length}건',
-            ),
+            Text('결과 ${searchResults.length}건'),
             Divider(),
             Expanded(
               child: ListView.builder(
@@ -132,7 +144,29 @@ class _NearFromMePageState extends State<NearFromMePage> {
 
   // 현재 위치로 지도 이동하는 함수
   Future<void> _goToCurrentPosition() async {
-    final GoogleMapController controller = await _controllerCompleter.future;
-    controller.animateCamera(CameraUpdate.newLatLng(_currentPosition));
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.medium,
+      );
+      if (position != null) {
+        setState(() {
+          _currentPosition = LatLng(position.latitude, position.longitude);
+        });
+        final GoogleMapController controller = await _controllerCompleter.future;
+        controller.animateCamera(CameraUpdate.newLatLng(_currentPosition!));
+      }
+    } catch (e) {
+      print('위치 가져오기 오류: $e');
+      // 위치 가져오기에 실패한 경우에 대한 처리 추가
+    }
+  }
+
+  // 위치 권한을 요청하는 함수
+  Future<void> _requestPermission() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // 사용자가 위치 권한을 거부한 경우
+      // 여기에 사용자에게 권한을 다시 요청하는 로직을 추가할 수 있습니다.
+    }
   }
 }
