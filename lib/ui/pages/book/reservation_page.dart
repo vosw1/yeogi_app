055e505273e76f0a08361d0fe054e79957e4d77c
vosw1 <vosw1@naver.com>
2024-05-09 12:53:17 +0900

    예약페이지 디자인 복구 완료

--- lib/ui/pages/book/reservation_page.dart
+++ lib/ui/pages/book/reservation_page.dart
@@ -102,7 +102,6 @@ class _ReservationPageState extends State<ReservationPage> {
               SizedBox(height: gap_s),
               Divider(),
               SizedBox(height: gap_m),
-              SizedBox(height: gap_m),
               Text(
                 "예약자 정보",
                 style: h5(),
@@ -196,31 +195,31 @@ class _ReservationPageState extends State<ReservationPage> {
         SizedBox(height: gap_s),
         Column(
           children: subtitle.map((text) {
-            return Row(
-              children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      Checkbox(
-                        value: _subCheckboxValues[subtitle.indexOf(text)],
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _subCheckboxValues[subtitle.indexOf(text)] =
-                                value ?? false;
-                          });
-                        },
-                      ),
-                      Text(text),
-                    ],
+            return GestureDetector(
+              onTap: () {
+                _showPopup(text); // 팝업을 표시하는 메소드를 호출
+              },
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Checkbox(
+                          value: _subCheckboxValues[subtitle.indexOf(text)],
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _subCheckboxValues[subtitle.indexOf(text)] =
+                                  value ?? false;
+                            });
+                          },
+                        ),
+                        Text(text),
+                      ],
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    _showPopup(context, text);
-                  },
-                  child: Icon(Icons.arrow_forward_ios),
-                ),
-              ],
+                  Icon(Icons.arrow_forward_ios),
+                ],
+              ),
             );
           }).toList(),
         ),
@@ -228,11 +227,11 @@ class _ReservationPageState extends State<ReservationPage> {
     );
   }
 
-  void _showPopup(BuildContext context, String text) {
+  void _showPopup(String text) {
     AlertDialog alertDialog;
 
     switch (text) {
-      case '이용규칙 및 취소/환불 규정 동의(필수)': // 수정된 부분
+      case '이용규칙 및 취소/환불 규정 동의(필수)':
         alertDialog = _buildUsageRulesDialog();
         break;
       case '개인정보 수집 및 이용 동의(필수)':
@@ -242,10 +241,10 @@ class _ReservationPageState extends State<ReservationPage> {
         alertDialog = _buildThirdPartyProvidingDialog();
         break;
       case '만 14세 이상 확인(필수)':
-        alertDialog = _buildAgeConfirmationDialog(context);
+        alertDialog = _buildAgeConfirmationDialog();
         break;
       default:
-        alertDialog = _buildDefaultDialog(context);
+        alertDialog = _buildDefaultDialog();
     }
 
     showDialog(
@@ -258,40 +257,24 @@ class _ReservationPageState extends State<ReservationPage> {
 
   AlertDialog _buildUsageRulesDialog() {
     return AlertDialog(
-      title: Text('이용규칙 및 취소/환불 동의', style: h6()),
+      title: Text('이용규칙 및 취소/환불 규정 동의'),
       content: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '이용규칙',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '19세 미만 청소년의 혼숙은 법적으로 불가하며, 이에 대한 숙소의 입실 거부 시 취소/환불이 불가합니다.\n'
-                  '업체 현장에서 객실 컨디션 및 서비스로 인해 발생된 분쟁은 여기어때에서 책임지지 않습니다.',
-            ),
-            SizedBox(height: 10),
-            Text(
-              '취소/환불 규정',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '취소/환불 가능 여부는 숙소의 정책을 따릅니다.\n'
-                  '취소/환불 규정에 따라 취소 수수료가 발생하는 경우, 취소 수수료는 판매가(상품가격) 기준으로 계산됩니다.',
-            ),
+        child: ListBody(
+          children: <Widget>[
+            Text('19세 미만 청소년의 혼숙은 법적으로 불가하며, 이에 대한 숙소의 입실 거부 시 취소/환불이 불가합니다.'),
+            Text('업체 현장에서 객실 컨디션 및 서비스로 인해 발생된 분쟁은 여기어때에서 책임지지 않습니다.'),
+            Text('취소/환불 가능 여부는 숙소의 정책을 따릅니다.'),
+            Text('취소/환불 규정에 따라 취소 수수료가 발생하는 경우, 취소 수수료는 판매가(상품가격) 기준으로 계산됩니다.')
           ],
         ),
       ),
-      backgroundColor: Colors.white,
+      actionsAlignment: MainAxisAlignment.center, // 버튼들을 중앙에 배치
       actions: <Widget>[
-        Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('확인'),
-          ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('확인'),
         ),
       ],
     );
@@ -299,45 +282,25 @@ class _ReservationPageState extends State<ReservationPage> {
 
   AlertDialog _buildPrivacyPolicyDialog() {
     return AlertDialog(
-      title: Text('개인정보 수집 및 이용 동의', style: h6()),
+      title: Text('개인정보 수집 및 이용 동의'),
       content: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '구분\t수집 목적\t수집 항목\t보유 및 이용기간',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '필수\t예약/구매 서비스 제공 상담 및 부정거래 기록 확인\t\n'
-                  '[예약·구매]\n'
-                  '예약자 정보(이름, 휴대전화번호)\n'
-                  '[결제]\n'
-                  '거래내역\n'
-                  '*결제 시 개인정보는 PG사(결제대행업체)에서 수집 및 저장하고 있으며, 회사는 PG사에서 제공하는 거래 내역만 제공받음\n'
-                  '[거래명세서 발급]\n'
-                  '이메일주소\n'
-                  '[현금영수증 발급]\n'
-                  '휴대전화번호, 이메일주소\n'
-                  '[취소·환불]\n'
-                  '은행명, 계좌번호, 예금주명\n'
-                  '- 회원 탈퇴 시 까지\n'
-                  '* 관계 법령에 따라 보존할 필요가 있는 경우 해당 법령에서 요구하는 기한까지 보유\n'
-                  '※ 위 동의 내용을 거부하실 수 있으나, 동의를 거부하실 경우 서비스를 이용하실 수 없습니다.\n'
-                  '※ 개인정보 처리와 관련된 상세 내용은 \'개인정보처리방침\'을 참고',
-            ),
+        child: ListBody(
+          children: <Widget>[
+            Text('구분\t수집 목적\t수집 항목\t보유 및 이용기간'),
+            Text('필수\t예약/구매 서비스 제공 상담 및 부정거래 기록 확인\t'),
+            Text('[예약·구매]\n예약자 정보(이름, 휴대전화번호)\n[결제]\n거래내역\n*결제 시 개인정보는 PG사(결제대행업체)에서 수집 및 저장하고 있으며, 회사는 PG사에서 제공하는 거래 내역만 제공받음\n[거래명세서 발급]\n이메일주소\n[현금영수증 발급]\n휴대전화번호, 이메일주소\n[취소·환불]\n은행명, 계좌번호, 예금주명\n- 회원 탈퇴 시 까지\n* 관계 법령에 따라 보존할 필요가 있는 경우 해당 법령에서 요구하는 기한까지 보유'),
+            Text('※ 위 동의 내용을 거부하실 수 있으나, 동의를 거부하실 경우 서비스를 이용하실 수 없습니다.'),
+            Text('※ 개인정보 처리와 관련된 상세 내용은 \'개인정보처리방침\'을 참고')
           ],
         ),
       ),
-      backgroundColor: Colors.white,
+      actionsAlignment: MainAxisAlignment.center,
       actions: <Widget>[
-        Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('확인'),
-          ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context). pop();
+          },
+          child: Text('확인'),
         ),
       ],
     );
@@ -345,85 +308,70 @@ class _ReservationPageState extends State<ReservationPage> {
 
   AlertDialog _buildThirdPartyProvidingDialog() {
     return AlertDialog(
-      title: Text('개인정보 제3자 제공 동의', style: h6()),
+      title: Text('개인정보 제3자 제공 동의'),
       content: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '제공받는 자\t${rooms.roomName}\n'
-                  '제공 목적\t숙박예약서비스 이용계약 이행\n'
-                  '(서비스 제공, 확인, 이용자 정보 확인)\n'
-                  '제공하는 항목\t예약한 숙박서비스의 이용자 정보(예약자 이름, 휴대폰번호, 예약번호, 예약 업체명, 예약한 객실명, 결제금액)\n'
-                  '제공받는 자의 개인정보\n'
-                  '보유 및 이용기간\t예약서비스 제공 완료 후 6개월\n'
-                  '※ 위 동의 내용을 거부하실 수 있으나, 동의를 거부하실 경우 서비스를 이용하실 수 없습니다.\n'
-                  '※ 개인정보 처리와 관련된 상세 내용은 \'개인정보처리방침\'을 참고',
-            ),
+        child: ListBody(
+          children: <Widget>[
+            Text('제공받는 자\t${rooms.roomName}'),
+            Text('제공 목적\t숙박예약서비스 이용계약 이행\n(서비스 제공, 확인, 이용자 정보 확인)'),
+            Text('제공하는 항목\t예약한 숙박서비스의 이용자 정보(예약자 이름, 휴대폰번호, 예약번호, 예약 업체명, 예약한 객실명, 결제금액)'),
+            Text('보유 및 이용기간\t예약서비스 제공 완료 후 6개월'),
+            Text('※ 위 동의 내용을 거부하실 수 있으나, 동의를 거부하실 경우 서비스를 이용하실 수 없습니다.'),
+            Text('※ 개인정보 처리와 관련된 상세 내용은 \'개인정보처리방침\'을 참고')
           ],
         ),
       ),
-      backgroundColor: Colors.white,
+      actionsAlignment: MainAxisAlignment.center,
       actions: <Widget>[
-        Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('확인'),
-          ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('확인'),
         ),
       ],
     );
   }
 
-  AlertDialog _buildAgeConfirmationDialog(BuildContext context) {
+  AlertDialog _buildAgeConfirmationDialog() {
     return AlertDialog(
-      title: Text('만 14세 이상 확인', style: h6()),
+      title: Text('만 14세 이상 확인'),
       content: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '여어떻노는 만 14세 미만 아동의 서비스 이용을 제한하고 있습니다.\n\n'
-                  '개인정보 보호법에는 만 14세 미만 아동의 개인정보 수집 시 법정대리인 동의를 받도록 규정하고 있으며,\n'
-                  '만 14세 미만 아동이 법정대리인 동의없이 서비스 이용이 확인된 경우 서비스 이용이 제한될 수 있음을 알려드립니다.',
-            ),
+        child: ListBody(
+          children: <Widget>[
+            Text('여어떻노는 만 14세 미만 아동의 서비스 이용을 제한하고 있습니다.'),
+            Text('개인정보 보호법에는 만 14세 미만 아동의 개인정보 수집 시 법정대리인 동의를 받도록 규정하고 있으며,'),
+            Text('만 14세 미만 아동이 법정대리인 동의없이 서비스 이용이 확인된 경우 서비스 이용이 제한될 수 있음을 알려드립니다.')
           ],
         ),
       ),
-      backgroundColor: Colors.white, // 배경색 변경
+      actionsAlignment: MainAxisAlignment.center,
       actions: <Widget>[
-        Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('확인'),
-          ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('확인'),
         ),
       ],
     );
   }
 
-  // 기본적인 AlertDialog를 반환하는 함수
-  AlertDialog _buildDefaultDialog(BuildContext context) {
+  AlertDialog _buildDefaultDialog() {
     return AlertDialog(
       title: Text('Notice'),
       content: Text('일치하는 팝업이 없습니다.'),
-      backgroundColor: Colors.white,
       actions: <Widget>[
-        Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('확인'),
-          ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('확인'),
         ),
       ],
     );
   }
+
   _showPopups(BuildContext context) {
     List<String> messages = [];
     if (!_subCheckboxValues.every((element) => element)) {
