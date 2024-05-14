3fbcfe34fe619fa26dee0856c46d75e103608b5a
vosw1 <vosw1@naver.com>
2024-05-14 18:05:37 +0900

    숙소리스트, 룸리스트 잉크웰로 적용

--- lib/ui/pages/stay/stay_list_pages/sale_list_page.dart
+++ lib/ui/pages/stay/stay_list_pages/sale_list_page.dart
@@ -20,8 +20,8 @@ class SaleStayListPage extends ConsumerWidget {
         appBar: AppBar(
           title: Text(
             '특가',
-            style:h4(),
-            ),
+            style: h4(),
+          ),
         ),
         body: Padding(
           padding: const EdgeInsets.only(bottom: gap_m),
@@ -40,20 +40,20 @@ class SaleStayListPage extends ConsumerWidget {
   Widget buildListItem(BuildContext context, Stay stay, int stayId) {
     return Padding(
       padding: const EdgeInsets.only(top: gap_m, left: gap_m, right: gap_m),
-      child: Row(
-        children: [
-          SizedBox(height: gap_xs),
-          Expanded(
-            flex: 1,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => StayDetailPage(stayId: stayId),
-                  ),
-                );
-              },
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => StayDetailPage(stayId: stayId),
+            ),
+          );
+        },
+        child: Row(
+          children: [
+            SizedBox(height: gap_xs),
+            Expanded(
+              flex: 1,
               child: Container(
                 height: 120, // 사진의 높이
                 decoration: BoxDecoration(
@@ -65,49 +65,49 @@ class SaleStayListPage extends ConsumerWidget {
                 ),
               ),
             ),
-          ),
-          SizedBox(width: 8), // 사진과 텍스트 사이 간격
-          Expanded(
-            flex: 2,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: double.infinity, // 화면 너비에 맞추기 위해 사용
-                  child: Text(
-                    stay.name,
-                    style: h5(),
+            SizedBox(width: 8), // 사진과 텍스트 사이 간격
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity, // 화면 너비에 맞추기 위해 사용
+                    child: Text(
+                      stay.name,
+                      style: h5(),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  SizedBox(height: gap_xx), // 텍스트 사이 간격
+                  Text(
+                    stay.address, // 숙소 위치
+                    style: TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
-                ),
-                SizedBox(height: gap_xx), // 텍스트 사이 간격
-                Text(
-                  stay.address, // 숙소 위치
-                  style: TextStyle(
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
+                  SizedBox(height: gap_s), // 텍스트 사이 간격
+                  Text(
+                    stay.intro, // 숙소 정보
+                    style: TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: gap_s), // 텍스트 사이 간격
-                Text(
-                  stay.intro, // 숙소 정보
-                  style: TextStyle(
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Colors.grey,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
