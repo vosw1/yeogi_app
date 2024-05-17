57e19278059443e05c7d09f5784a3dc48d0a7784
vosw1 <vosw1@naver.com>
2024-05-17 19:09:18 +0900

    달력 선택

--- lib/ui/pages/room/widgets/reservation_calendar.dart
+++ lib/ui/pages/room/widgets/reservation_calendar.dart
@@ -32,90 +32,103 @@ class _ReservationCalendarState extends State<ReservationCalendar> {
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      locale: 'ko_KR', // 한글 설정
-      focusedDay: _tempStart ?? DateTime.now(),
-      firstDay: DateTime.utc(2000, 1, 1),
-      lastDay: DateTime.utc(2100, 12, 31),
-      rangeSelectionMode: RangeSelectionMode.toggledOff,
-      selectedDayPredicate: (day) {
-        return isSameDay(widget.selectedStartDate, day) || isSameDay(widget.selectedEndDate, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          if (_tempStart == null || (_tempStart != null && _tempEnd != null)) {
-            _tempStart = selectedDay;
-            _tempEnd = null;
-          } else if (_tempStart != null && _tempEnd == null) {
-            if (selectedDay.isBefore(_tempStart!)) {
-              _tempEnd = _tempStart;
-              _tempStart = selectedDay;
-            } else {
-              _tempEnd = selectedDay;
-            }
-            widget.onRangeSelected(_tempStart!, _tempEnd!);
-          } else if (_tempStart != null && _tempEnd != null) {
-            _tempStart = selectedDay;
-            _tempEnd = null;
-          }
-        });
-      },
-      enabledDayPredicate: (day) {
-        return !_isReserved(day) && !_isPast(day);
-      },
-      calendarBuilders: CalendarBuilders(
-        defaultBuilder: (context, day, focusedDay) {
-          if (_isReserved(day)) {
-            return Center(
-              child: Text(
-                '${day.day}',
-                style: TextStyle(color: Colors.grey.withOpacity(0.5)), // 회색으로 설정
-              ),
-            );
-          } else if (_isPast(day)) {
-            return Center(
-              child: Text(
-                '${day.day}',
-                style: TextStyle(color: Colors.grey.withOpacity(0.5)), // 회색으로 설정
-              ),
-            );
-          }
-          return null;
+    return Container(
+      child: TableCalendar(
+        locale: 'ko_KR',
+        // 한글 설정
+        focusedDay: _tempStart ?? DateTime.now(),
+        firstDay: DateTime.utc(2000, 1, 1),
+        lastDay: DateTime.utc(2100, 12, 31),
+        rangeSelectionMode: RangeSelectionMode.toggledOff,
+        selectedDayPredicate: (day) {
+          return isSameDay(widget.selectedStartDate, day) ||
+              isSameDay(widget.selectedEndDate, day);
         },
-        rangeStartBuilder: (context, date, _) => Container(
-          margin: const EdgeInsets.all(4.0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.redAccent,
-            borderRadius: BorderRadius.circular(10.0),
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            if (_tempStart == null && _tempEnd == null) {
+              // 첫 번째 날짜를 선택할 때
+              _tempStart = selectedDay;
+            } else if (_tempStart != null && _tempEnd == null) {
+              // 두 번째 날짜를 선택할 때
+              if (selectedDay.isBefore(_tempStart!)) {
+                // 두 번째로 선택한 날짜가 첫 번째 날짜보다 이전인 경우
+                _tempEnd = _tempStart;
+                _tempStart = selectedDay;
+              } else {
+                // 두 번째로 선택한 날짜가 첫 번째 날짜보다 나중인 경우
+                _tempEnd = selectedDay;
+              }
+              // 범위 선택이 완료되면 onRangeSelected 호출
+              widget.onRangeSelected(_tempStart!, _tempEnd!);
+            } else if (_tempStart != null && _tempEnd != null) {
+              // 새로운 범위 선택을 시작할 때
+              _tempStart = selectedDay;
+              _tempEnd = null;
+            }
+            // 클릭할 때마다 선택된 날짜를 반영
+            widget.onRangeSelected(_tempStart!, _tempEnd ?? _tempStart!);
+          });
+        },
+        enabledDayPredicate: (day) {
+          return !_isReserved(day) && !_isPast(day);
+        },
+        calendarBuilders: CalendarBuilders(
+          defaultBuilder: (context, day, focusedDay) {
+            if (_isReserved(day)) {
+              return Center(
+                child: Text(
+                  '${day.day}',
+                  style:
+                      TextStyle(color: Colors.grey.withOpacity(0.5)), // 회색으로 설정
+                ),
+              );
+            } else if (_isPast(day)) {
+              return Center(
+                child: Text(
+                  '${day.day}',
+                  style:
+                      TextStyle(color: Colors.grey.withOpacity(0.5)), // 회색으로 설정
+                ),
+              );
+            }
+            return null;
+          },
+          rangeStartBuilder: (context, date, _) => Container(
+            margin: const EdgeInsets.all(4.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.redAccent,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text(
+              '${date.day}',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-          child: Text(
-            '${date.day}',
-            style: TextStyle(color: Colors.white),
+          rangeEndBuilder: (context, date, _) => Container(
+            margin: const EdgeInsets.all(4.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.redAccent,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text(
+              '${date.day}',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-        rangeEndBuilder: (context, date, _) => Container(
-          margin: const EdgeInsets.all(4.0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.redAccent,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Text(
-            '${date.day}',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        withinRangeBuilder: (context, date, _) => Container(
-          margin: const EdgeInsets.all(4.0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.redAccent.shade100,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Text(
-            '${date.day}',
-            style: TextStyle(color: Colors.white),
+          withinRangeBuilder: (context, date, _) => Container(
+            margin: const EdgeInsets.all(4.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.redAccent.shade100,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text(
+              '${date.day}',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
