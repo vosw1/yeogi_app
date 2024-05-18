559337227ac5ddeacd1dd3000c3382c5030f9c9b
vosw1 <vosw1@naver.com>
2024-05-18 16:05:06 +0900

    찜 페이지 로그인 후 이용하게 설정

--- lib/ui/pages/scrap/widgets/scrap_result_list.dart
+++ lib/ui/pages/scrap/widgets/scrap_result_list.dart
@@ -9,34 +9,33 @@ class ScrapResultList extends ConsumerWidget {
     final viewModel = ref.read(scrapListProvider.notifier);
     ScrapListModel? model = ref.watch(scrapListProvider);
 
-    if(model == null){
+    if (model == null) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
         viewModel.notifyInit();
       });
-    }
 
-    if (model == null) {
+      // Return the login required message directly in the widget tree
       return Center(
         child: Text(
-          "찜 목록이 비어있습니다",
+          '로그인을 먼저 해주세요.',
           style: TextStyle(fontSize: 20),
         ),
       );
-    } else {
-      return ListView.builder(
-        shrinkWrap: true,
-        itemCount: model.scraps.length,
-        itemBuilder: (context, index) {
-          return Container(
-            constraints: BoxConstraints(
-              maxHeight: 130,
-            ),
-            child: ScrapListItem(
-              scrap: model.scraps[index],
-            ),
-          );
-        },
-      );
     }
+
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: model.scraps.length,
+      itemBuilder: (context, index) {
+        return Container(
+          constraints: BoxConstraints(
+            maxHeight: 130,
+          ),
+          child: ScrapListItem(
+            scrap: model.scraps[index],
+          ),
+        );
+      },
+    );
   }
 }
