b5890efda29cf3dc3ac474575c0d98a5e3cea148
seoldonghun <ehdgns5647@gmail.com>
2024-05-18 15:34:02 +0900

    scrap 완료 (로그아웃 제ì외)

--- lib/ui/pages/scrap/scrap_list_page.dart
+++ lib/ui/pages/scrap/scrap_list_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yogi_project/_core/constants/size.dart';
-import 'package:yogi_project/data/store/session_store.dart';
-import 'package:yogi_project/ui/pages/scrap/scrap_list_view_model.dart';
 import 'package:yogi_project/ui/pages/scrap/widgets/scrap_result_list.dart';
 
 class ScrapListPage extends StatelessWidget {
