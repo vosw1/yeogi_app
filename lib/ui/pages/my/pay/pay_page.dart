b6f1962cab74586e366b8f5826cb19134920e07b
vosw1 <vosw1@naver.com>
2024-05-18 18:12:20 +0900

    미로그인시 로그인 페이지로 이동하게 설정

--- lib/ui/pages/my/pay/pay_page.dart
+++ lib/ui/pages/my/pay/pay_page.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
-import 'package:yogi_project/_core/constants/http.dart';
-import 'package:yogi_project/data/dtos/pay_request.dart';
 import 'package:yogi_project/data/dtos/reservation_request.dart';
-import 'package:yogi_project/data/repositories/pay_repository.dart';
 import 'package:yogi_project/data/store/session_store.dart';
-import 'package:bootpay/bootpay.dart';
-import 'package:bootpay/model/extra.dart';
-import 'package:bootpay/model/item.dart';
-import 'package:bootpay/model/payload.dart';
-import 'package:bootpay/model/user.dart';
+import 'package:yogi_project/ui/pages/my/pay/widgets/pay_button.dart';
+import 'package:yogi_project/ui/pages/my/reservation/my_reservation_page.dart';
 
 class PayPage extends ConsumerWidget {
   final ReservationSaveReqDTO reservations;
@@ -23,23 +16,15 @@ class PayPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final sessionStore = ref.read(sessionProvider);
-    final payRepositoryProvider = Provider((ref) => PayRepository(dio, Logger()));
-    final payRepository = ref.read(payRepositoryProvider);
 
-    void handlePayment() async {
-      // 결제 금액이 최소 100원 이상이 되도록 설정
-      int amount = reservations.price < 100 ? 100 : reservations.price;
-
-      PaySaveReqDTO payInfo = PaySaveReqDTO(
-        payId: reservations.roomId,
-        reservationId: reservations.roomId, // 적절한 예약 ID로 수정 필요
-        amount: amount,
-        way: "Credit Card",
-        state: "COMPLETE",
-        payAt: DateTime.now(),
-      );
-
-      bootpayTest(context, payInfo);
+    void onPaymentDone() {
+      Future.delayed(Duration(seconds: 2), () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => MyReservationPage(users: sessionStore.user!, eventMyPageBanners: [],)),
+              (Route<dynamic> route) => false,
+        );
+      });
     }
 
     return Scaffold(
@@ -67,18 +52,10 @@ class PayPage extends ConsumerWidget {
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: SizedBox(
-                width: double.infinity, // 가로로 전체를 채우도록 설정
-                child: ElevatedButton(
-                  onPressed: handlePayment,
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.all(12),
-                    backgroundColor: Colors.redAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: Text('결제하기',
-                      style: TextStyle(fontSize: 16.0, color: Colors.white)),
+                width: double.infinity,
+                child: PayButton(
+                  reservations: reservations,
+                  onPaymentDone: onPaymentDone,
                 ),
               ),
             ),
@@ -99,69 +76,5 @@ class PayPage extends ConsumerWidget {
         ),
       ),
     );
-  }
-
-  void bootpayTest(BuildContext context, PaySaveReqDTO payInfo) {
-    Payload payload = getPayload(payInfo);
-    Bootpay().requestPayment(
-      context: context,
-      payload: payload,
-      showCloseButton: true,
-      onCancel: (String data) {
-        print('------- onCancel: $data');
-        Bootpay().dismiss(context); // 명시적으로 부트페이 뷰 종료 호출
-      },
-      onError: (String data) {
-        print('------- onError: $data');
-      },
-      onClose: () {
-        print('------- onClose');
-        Bootpay().dismiss(context); // 명시적으로 부트페이 뷰 종료 호출
-      },
-      onIssued: (String data) {
-        print('------- onIssued: $data');
-      },
-      onConfirm: (String data) {
-        print('------- onConfirm: $data');
-        return true;
-      },
-      onDone: (String data) {
-        print('------- onDone: $data');
-        // 결제가 완료되었을 때 추가 작업
-      },
-    );
-  }
-
-  Payload getPayload(PaySaveReqDTO payInfo) {
-    Payload payload = Payload();
-    Item item = Item();
-    item.name = reservations.reservationName; // 주문정보에 담길 상품명
-    item.qty = 1; // 해당 상품의 주문 수량
-    item.id = reservations.roomId.toString(); // 해당 상품의 고유 키
-    item.price = payInfo.amount.toDouble(); // 상품의 가격
-
-    payload.webApplicationId = 'your-web-5b8f6a4d396fa665fdc2b5e7-id';
-    payload.androidApplicationId = '5b8f6a4d396fa665fdc2b5e8';
-    payload.iosApplicationId = '5b8f6a4d396fa665fdc2b5e9';
-
-    payload.pg = '나이스페이';
-    payload.orderName = reservations.reservationName; // 결제할 상품명
-    payload.price = payInfo.amount.toDouble(); // 결제 금액
-
-    payload.orderId = DateTime.now().millisecondsSinceEpoch.toString(); // 주문번호
-
-    payload.items = [item]; // 상품정보 배열
-
-    User user = User(); // 구매자 정보
-    user.username = reservations.reservationName;
-    user.phone = reservations.reservationTel;
-
-    Extra extra = Extra(); // 결제 옵션
-    extra.appScheme = 'bootpayFlutterExample';
-    extra.cardQuota = '3';
-
-    payload.user = user;
-    payload.extra = extra;
-    return payload;
   }
 }
